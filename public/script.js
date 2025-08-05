0a6d55582ac32c8ff5d4f808ced9ca8a4c75fa99
liannegeiste19 <b2029lepgesite@pshs.edu.ph>
2025-08-05 08:57:50 +0800

    first non graded

--- public/script.js
+++ public/script.js
@@ -1,1 +1,8 @@
 // no script tag needed
+
+var age = 14
+var maxAge = 100
+var numPerDay = 3
+
+totalRequired = (numPerDay * 365) * (maxAge - age); 
+document.write("You will need " + totalRequired + " to last you until the ripe old age of " + maxAge);
