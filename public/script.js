4e716ad3ee98224c867efc359f40ea291b253732
liannegeiste19 <b2029lepgesite@pshs.edu.ph>
2025-08-12 08:30:50 +0800

    speed test one

--- public/script.js
+++ public/script.js
@@ -1,16 +1,12 @@
 // no script tag needed
 
 
-var x = prompt("Eneter age" , "0");
-var y = prompt("Eneter maxAge" , "0");
-var z = prompt("Eneter amount of times you eat your fav snacks per day" , "0");
+let age = prompt("Eneter age" , "14");
+let maxAge = prompt("Eneter maxAge" , "100");
+let numPerDay = prompt("Eneter amount of times you eat your fav snacks per day" , "3");
 
-age = 14
-maxAge = 100
-numPerDay = 3
-totalRequired = maxAge*numPerDay; 
+totalRequired = (Number(numPerDay) * 365 * (Number(maxAge) - Number(age)));
 
-let name = window.prompt("Enter ", "Your Name");
 if (confirm('Are you sure?')){
     console.log("You will need " + totalRequired + " to last you until the ripe old age of " + maxAge);
 }
